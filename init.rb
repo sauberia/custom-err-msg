8723c298cc62565d2ed81ba13326663e3e3163f4
Sean Sawyer <ssawyer@vitrue.com>
2010-09-20 14:01:30 -0400

    Redefine full_messages on ActiveModel::Errors instead of ActiveRecord to support Rails 3

--- init.rb
+++ init.rb
@@ -1,4 +1,4 @@
 require 'custom_error_message'
 
-ActiveRecord::Errors.send(:include, CustomErrorMessage)
+ActiveModel::Errors.send(:include, CustomErrorMessage)
 ActiveResource::Errors.send(:include, CustomErrorMessage)
